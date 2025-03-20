8d21bc7d2b72217fd37a8555e4863e3456259d42
TianmuTNT <110798004+TianmuTNT@users.noreply.github.com>
2025-03-21 00:47:37 +0800

    优化

--- auto_twitch.py
+++ auto_twitch.py
@@ -11,11 +11,11 @@ num_threads = 5
 def generate():
     return "".join(choices(ascii_letters + digits, k=10))
 
-def get_code(g, num):
+def get_code(g, num, time=30):
     host = "pop.yourmail.com"
     port = 995
-    username = "you@yourmail.com"
-    password = "yourpassword" # 这里几项要改，邮箱开启pop3
+    username = "yourname@yourmail.com"
+    password = "yourpassword"
     pop_server = POP3_SSL(host, port)
     pop_server.user(username)
     pop_server.pass_(password)
@@ -34,8 +34,12 @@ def get_code(g, num):
         except Exception:
             continue
     pop_server.quit()
-    sleep(1)
-    return get_code(g, num)
+    time -= 1
+    if time == 0:
+        raise TimeoutError("Get captcha code timeout")
+    else:
+        sleep(1)
+        return get_code(g, num, time)
 
 def get_cape():
     with sync_playwright() as p:
@@ -49,20 +53,20 @@ def get_cape():
 
                 page.locator("[data-a-target='signup-button']").click()
                 g = generate()
-                print(f"({current_thread().name}) {g}")
+                print(f"({current_thread().name}) Name: {g}")
                 page.locator("[id='signup-username']").fill(g)
                 page.locator("[id='password-input']").fill(g)
                 page.locator("xpath=/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div/div[2]/form/div/div[3]/div/div[2]/div[1]/div/select").select_option("2000")
                 page.locator("[data-a-target='birthday-month-select']").select_option("1")
                 page.locator("[data-a-target='birthday-date-input']").select_option("1")
                 page.locator("[data-a-target='signup-phone-email-toggle']").click()
-                page.locator("[id='email-input']").fill(g+"@yourdomain.com") # 这里要改成你域名，cf配置转发
+                page.locator("[id='email-input']").fill(g+"@yourdomain.com")
                 page.locator("[data-a-target='passport-signup-button']").click()
 
                 code_input = page.get_by_role("textbox", name="数字 1")
                 code_input.wait_for()
                 code = get_code(g, num_threads)
-                print(f"({current_thread().name}) {code}")
+                print(f"({current_thread().name}) Captcha code: {code}")
                 code_input.fill(code)
 
                 setting_button = page.get_by_role("button", name="设置", exact=True)
@@ -76,12 +80,12 @@ def get_cape():
                 code_text = page.locator("#root a").filter(has_text="您获得了一份 Mojang 的奖励：Home Cape")
                 code_text.wait_for(timeout=300000)
                 redeem_code = code_text.text_content().split("换：")[1].split("。")[0]
-                print(f"({current_thread().name}) {redeem_code}")
+                print(f"({current_thread().name}) Redeem code: {redeem_code}")
                 with open("redeem_codes.txt", "a") as f:
                     f.write(redeem_code + "\n")
                     f.close()
             except Exception as e:
-                print(f"({current_thread().name}) {e}")
+                print(f"({current_thread().name}) ERROR: {e}")
             finally:
                 context.close()
 
